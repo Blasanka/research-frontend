b513638fcfa8d1bd937493a50051b71162cd41ea
Malindu Kirindegama <IT18041880@my.sliit.lk>
2021-10-06 21:32:17 +0530

    User authentication firebase and recommendation system with updated UI components and gradle with manifest

---
+++ app/src/main/java/com/example/floral/ApiInterface.java
@@ -0,0 +1,12 @@
+//package com.example.floral;
+//
+//import com.example.testfloral.Growth;import java.util.List;
+//
+//import retrofit2.Call;
+//import retrofit2.http.GET;
+//
+//public interface ApiInterface {
+//    @GET("api/v1.0/recommendations/3")
+//    Call<List<Growth>> getGrowthInfo();
+//
+//}
--- app/src/main/java/com/example/floral/suggestSystem/data/Field.kt
+++
@@ -1,7 +0,0 @@
-package com.example.floral.suggestSystem.data
-
-
-data class Field(
-    val name: String,
-    val type: String
-)
