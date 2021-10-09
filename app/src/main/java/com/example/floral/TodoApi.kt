ba424cb826a8ad5276bffa6e64b8e6a793ef5358
Malindu Kirindegama <IT18041880@my.sliit.lk>
2021-10-10 05:29:15 +0530

    Updated Api Interface with Retrofit

--- app/src/main/java/com/example/floral/TodoApi.kt
+++ app/src/main/java/com/example/floral/TodoApi.kt
@@ -1,5 +1,8 @@
-package com.example.testfloral
+package com.example.floral
 
+import com.example.testfloral.FavList
+import com.example.testfloral.RacAlgorithm
+import com.example.testfloral.Todo
 import retrofit2.Response
 import retrofit2.http.GET
 
@@ -9,10 +12,13 @@ interface TodoApi {
     suspend fun getTodos(): Response<List<Todo>>
 
     //3,4,7
-    @GET("/api/v1.0/recommendations/3")
+    @GET("/api/v1.0/recommendations/7")
     suspend fun getRec(): Response<List<RacAlgorithm>>
 
     @GET("/api/v1.0/fav/3")
     suspend fun getFav(): Response<List<FavList>>
+
+    @GET("/api/v1.0/writer/7::2081::7::978298504")
+    suspend fun getwri(): Response<List<FavList>>
 
 }
