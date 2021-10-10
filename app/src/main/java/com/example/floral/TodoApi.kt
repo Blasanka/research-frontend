afef48cd1bd25e65fed72baab0137c594d88ab4a
Malindu Kirindegama <IT18041880@my.sliit.lk>
2021-10-10 10:07:44 +0530

    Added Attribites to Retrofit

--- app/src/main/java/com/example/floral/TodoApi.kt
+++ app/src/main/java/com/example/floral/TodoApi.kt
@@ -5,6 +5,7 @@ import com.example.testfloral.RacAlgorithm
 import com.example.testfloral.Todo
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface TodoApi {
 
@@ -12,13 +13,15 @@ interface TodoApi {
     suspend fun getTodos(): Response<List<Todo>>
 
     //3,4,7
-    @GET("/api/v1.0/recommendations/7")
-    suspend fun getRec(): Response<List<RacAlgorithm>>
+    @GET("/api/v1.0/recommendations/{Id}")
+    suspend fun getRec(@Path("Id") employeeId: String): Response<List<RacAlgorithm>>
 
-    @GET("/api/v1.0/fav/3")
+    @GET("/api/v1.0/fav/{Id}")
     suspend fun getFav(): Response<List<FavList>>
 
-    @GET("/api/v1.0/writer/7::2081::7::978298504")
-    suspend fun getwri(): Response<List<FavList>>
+    @GET("/api/v1.0/writer/{Id}")
+    suspend fun getwri(@Path("Id") employeeId: String): Response<List<FavList>>
 
 }
+
+
